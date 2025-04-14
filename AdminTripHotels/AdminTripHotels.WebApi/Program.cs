12ed9107b984f733f322f3c1d8c5127dc371fdb8
Maxim Mineev <mms1401@ya.ru>
2025-04-14 15:08:32 +0500

    hotel info service, hotel info controller

--- AdminTripHotels/AdminTripHotels.WebApi/Program.cs
+++ AdminTripHotels/AdminTripHotels.WebApi/Program.cs
@@ -2,22 +2,27 @@ using AdminTripHotels.Core;
 using AdminTripHotels.Core.Domain;
 using AdminTripHotels.Core.Services;
 using AdminTripHotels.WebApi.DTO;
+using AdminTripHotels.Core.Repositories;
+using AdminTripHotels.WebApi.DTO.HotelInfo;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddAdminTripHotelsCore();
 builder.Services.AddScoped<IOfferService, OfferService>();
+builder.Services.AddScoped<IHotelInfoService, HotelInfoService>();
+
+builder.Services.AddScoped<IRepository<HotelInfo>, HotelInfoRepository>();
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAutoMapper(config =>
-{
-	config.CreateMap<HotelOffer, OfferDTO>();
-});
+builder.Services.AddAutoMapper(config => { config.CreateMap<HotelOffer, OfferDTO>(); });
+builder.Services.AddAutoMapper(config => { config.CreateMap<HotelInfo, HotelInfoDefaultDTO>(); });
+builder.Services.AddAutoMapper(config => { config.CreateMap<HotelInfo, HotelInfoGetAllDTO>(); });
+builder.Services.AddAutoMapper(config => { config.CreateMap<HotelInfoDefaultDTO, HotelInfo>(); });
 
 var app = builder.Build();
 app.UseHttpsRedirection();
